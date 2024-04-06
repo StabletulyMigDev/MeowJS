f745655561c11170efd7883840022c0df0b17e44
Miguel Vicente <166228817+StabletulyMigDev@users.noreply.github.com>
2024-04-06 10:45:56 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+function _cli() {
+  const { program } = require("commander");
+  program
+  .name('MeowJS cli utility')
+  .version('0.0.6')
+  .action(function() {
+    // some code here
+  })
+}
